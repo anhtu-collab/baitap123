97ae7991da4b13cef5039b15071f5a7a4886e6f4
Tu <tu.beo.vui@gmail.com>
2026-03-04 20:51:43 +0700

    cap nhap chuc nang dang xuat va giao dien admi va co ban giao dien useri

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -1,7 +1,6 @@
 <?php
 
 namespace App\Http\Controllers\Auth;
-
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 
@@ -25,6 +24,8 @@ class LoginController extends Controller
      *
      * @var string
      */
+    protected $redirectTo = '/';
+
     /**
      * Create a new controller instance.
      *
--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->string('utype')->default('USER')->comment('ADM for Admin and USD for User or Custumer');
+            $table->string('utype')->default('USER')->comment('ADM for Admin and USR for User or Custumer');
             $table->rememberToken();
             $table->timestamps();
         });
